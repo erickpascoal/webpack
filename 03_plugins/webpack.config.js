63771be3bb4417b2e80fa510604f5ad45a1b1ffe
Erick Pascoal <erix_ps@hotmail.com>
2021-12-08 19:58:35 -0300

    gera nomes com hashs para nosso bundle

--- 03_plugins/webpack.config.js
+++ 03_plugins/webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
+    filename: "[name]-[contenthash].js",
   },
   mode: "development",
   //   optimization: {
@@ -26,7 +26,7 @@ module.exports = {
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: "styles.css",
+      filename: "[name]-[contenthash].css",
     }),
     new webpack.DefinePlugin({
       VERSION: JSON.stringify("1.0.2"),
