8b3747dc85b71580e165c70c8c1709d0960cd2a7
Erick Pascoal <erix_ps@hotmail.com>
2021-12-08 12:43:16 -0300

    implementa babel e plugin de spread operators

--- 02_loaders/src/index.js
+++ 02_loaders/src/index.js
@@ -12,3 +12,15 @@ logo.apply();
 
 const button = new Button();
 button.create("Clique aqui");
+
+const teste = { a: 1, b: 2, c: 3, d: 4 };
+
+const { a, b, ...rest } = teste;
+
+console.log(a);
+console.log(b);
+console.log(rest);
+
+const teste2 = { ...teste };
+
+console.log(teste2);
