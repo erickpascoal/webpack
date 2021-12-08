ad75676ae51cc70aed0362fa9109e115c3a1991c
Erick Pascoal <erix_ps@hotmail.com>
2021-12-08 14:44:37 -0300

    implementa loader para HTML

--- 02_loaders/src/index.js
+++ 02_loaders/src/index.js
@@ -1,26 +1,29 @@
 import Button from "./components/Button";
 import Logo from "./components/Logo";
 import Title from "./components/Ttitle";
+import warningTemplate from "./templates/warning.html";
+import "./styles/warning.scss";
 
+// loader de css
 const title = new Title();
-
 title.create("Erick Pascoal dos Santos");
 
+//loader de imagem
 const logo = new Logo();
-
 logo.apply();
 
+//loader de sass
 const button = new Button();
 button.create("Clique aqui");
 
+//spread com babel
 const teste = { a: 1, b: 2, c: 3, d: 4 };
-
 const { a, b, ...rest } = teste;
-
 console.log(a);
 console.log(b);
 console.log(rest);
-
 const teste2 = { ...teste };
-
 console.log(teste2);
+
+//loader de html
+document.querySelector("body").innerHTML += warningTemplate;
--- 02_loaders/webpack.config.js
+++ 02_loaders/webpack.config.js
@@ -32,6 +32,10 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.html$/,
+        use: "html-loader",
+      },
     ],
   },
 };
