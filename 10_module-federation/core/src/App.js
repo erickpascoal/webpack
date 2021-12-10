dbd9b69735535ae52fe2160760e0453b216e3c7d
Erick Pascoal <erix_ps@hotmail.com>
2021-12-10 10:53:30 -0300

    inicia modulo de module federation

---
+++ 10_module-federation/core/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function App() {
+  return (
+    <div>
+      <h1>Essa é a Core</h1>
+    </div>
+  );
+}
---
+++ 10_module-federation/core/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  mode: "development",
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 9001,
+    compress: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        options: {
+          presets: ["@babel/preset-react"],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+    }),
+  ],
+};
