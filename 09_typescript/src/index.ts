a02507ccceb0b38565daf154d3ec516a54f92327
Erick Pascoal <erix_ps@hotmail.com>
2021-12-09 22:24:37 -0300

    integra typescript a um projeto com webpack

---
+++ 09_typescript/src/index.ts
@@ -0,0 +1,14 @@
+const teste: number = 2;
+
+type Person = {
+  name: string;
+  age: number;
+};
+
+const person: Person = {
+  name: "Erick Pascoal",
+  age: 26,
+};
+
+console.log(teste);
+console.log(person);
---
+++ 09_typescript/webpack.config.js
@@ -0,0 +1,18 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.ts",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+      },
+    ],
+  },
+};
